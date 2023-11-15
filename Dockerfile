68085e7d90aa28dbf7ddc58609ad4d3fca9788ba
ramir512 <ramir.khvan@gmail.com>
2023-11-15 15:49:11 +0300

    Improve linkextractor

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3
 RUN pip install beautifulsoup4
 RUN pip install requests
 
+
 WORKDIR /app
 COPY linkextractor.py /app/
 RUN chmod a+x linkextractor.py
--- linkextractor.py
+++ linkextractor.py
@@ -3,8 +3,24 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-response = requests.get(sys.argv[-1])
-soup = BeautifulSoup(response.text, "html.parser")
-for link in soup.find_all("a"):
-    print(link.get("href"))
+def extract_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    base = url
+    links = []
+    for link in soup.find_all("a"):
+        links.append({
+            "text": " ".join(link.text.split()) or ["IMG"],
+            "href": urljoin(base, link.get("href"))
+        }
+    )
+    return links
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("\nUsage: \n\t{} <URL>\n".format(sys.argv[0]))
+        sys.exit(1)
+    for link in extract_links(sys.argv[-1]):
+        print("[{}]({})".format(link["text"], link["href"]))
